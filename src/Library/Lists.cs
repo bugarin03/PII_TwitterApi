3c001ff682fef88e4fdcd57377aefd950337f70f
bugarin03 <bruno.bugarin@correo.ucu.edu.uy.com>
2022-10-08 15:55:37 -0300

    s

--- src/Library/Lists.cs
+++ src/Library/Lists.cs
@@ -1,7 +1,40 @@
+using System.Collections.Generic;
+using System.Collections;
 namespace UcuUber
 {
-    public class Lists
+    public class Info
     {
+        private static Info data;
+        private Info()
+        {
+
+        }
+    
+        public static Info Data
+        {
+            get
+            {
+                if (data == null)
+                {
+                    data = new Info();
+                }
+                return data;
+            }
+        }
         
+        public List<Driver> FreeNormalDrivers { get; set; } = new List<Driver>();
+
+        public List<Driver> FreePoolDrivers { get; set; } = new List<Driver>();
+
+        public List<Driver> BusyNormalDrivers { get; set; } = new List<Driver>();
+
+        public List<Driver> BusyPoolDrivers { get; set; } = new List<Driver>();
+
+        public List<Passanger> Passangers { get; set; } = new List<Passanger>();
+
+        public  void Movement (Driver driver, Passanger passanger )
+        {
+            
+        }
     }
 }
---
+++ src/Library/Passanger.cs
@@ -0,0 +1,10 @@
+namespace UcuUber
+{
+    /// <summary>
+    /// Esta clase se encarga de calcular la reputación tanto de un conductor como de un pasajero.
+    /// </summary>
+    public class Passanger : Person
+    {
+       
+    }
+}
